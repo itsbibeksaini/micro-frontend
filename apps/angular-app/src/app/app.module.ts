11f6654010fb9bf7d53aa7ffdae1979c9e3935f0
Bibek Saini <itsbibeksaini@gmail.com>
2021-06-08 22:40:03 +0530

    Adding angular single-spa app

---
+++ apps/angular-app/src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { APP_BASE_HREF } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { EmptyRouteComponent } from './empty-route/empty-route.component';
+
+@NgModule({
+  declarations: [AppComponent, EmptyRouteComponent],
+  imports: [BrowserModule, AppRoutingModule],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
